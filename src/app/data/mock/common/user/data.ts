5edfe8f1ea099e1888cbbbc7f9d8676c0d1cc0ea
Cyben Lainez <cybenlainez@gmail.com>
2020-08-11 22:00:35 +0800

    Profile - Names

--- src/app/data/mock/common/user/data.ts
+++ src/app/data/mock/common/user/data.ts
@@ -1,8 +1,8 @@
 /* tslint:disable:max-line-length */
 export const user: any = {
     id    : 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
-    name  : 'Andrew Watkins',
-    email : 'watkins.andrew@company.com',
-    avatar: 'assets/images/avatars/andrew-watkins.jpg',
+    name  : 'Jonathan Barcelona',
+    email : 'jonathan.barcelona@gmail.com',
+    avatar: 'assets/images/avatars/jonathan-barcelona.jpg',
     status: 'online'
 };
